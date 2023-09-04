032941358063f66df2ac6e0d96704fd6cf932e6e
grumstv <grumstv@gmail.com>
2023-09-04 22:53:46 +0300

    Update JiraSearch.js

--- ChMAF/Modules/JiraSearch.js
+++ ChMAF/Modules/JiraSearch.js
@@ -205,10 +205,7 @@ function getJiraTask() { // функция получения таски джи�
 					`<img src="${rezissuetable.issueTable.table.match(/https:\/\/jira.skyeng.tech\/images\/icons\/priorities\/.*svg/gm)[i]}" style="width:20px; height:25px;" title="Приоритеты: ⛔ - Blocker, полностью залитая красная стрелка вверх - Critical, три красные стрелки вверх - Major, три синие вниз - Minor, ⭕ - Trivial">` + 
 					' ' + `<a name="buglinks" href="https://jira.skyeng.tech/browse/${issueKeys[i]}" onclick="" target="_blank" style="margin-left:5px; color: #ffe4c4">` + temporarka + '</a>' + 
 					`<span name="issueIds" style="display:none">${rezissuetable.issueTable.issueIds[i]}</span>` + 
-					'<span class = "jiraissues" style="margin-left: 10px; cursor: pointer">💬</span>' + 
-					'<span class = "refreshissues" style="color:#ADFF2F; margin-left: 5px; cursor: pointer">&#69717;&#120783;</span>' + 
-					'<span name="addtofavourites" style="cursor:pointer;" title="Добавить задачу в Избранное">🤍</span>' + 
-					'</br>';
+					'<span class = "jiraissues" style="margin-left: 10px; cursor: pointer">💬</span>' + '</br>';
 			} else {
 				console.error("Не удалось найти соответствие для индекса: " + i);
 			}
@@ -408,8 +405,6 @@ function switchJiraPages() {
 									
 									
 									for (let i = 0; i < rezissuetable.issueTable.displayed; i++) {
-
-										if (rezissuetable.issueTable.issueKeys[i] != undefined) {
 										
 										function filterItems1(item, index) {
 											return index % 2 != 0 ? item : null;
@@ -440,26 +435,69 @@ function switchJiraPages() {
 											temporarka = replaceItem1(matchedItems1[i]);
 										}
 										
-										if (issueKeys[i] != undefined) {
-										
-										issues += '<span style="color: #00FA9A">&#5129;</span>' + 
-										`<img src="${rezissuetable.issueTable.table.match(/https:\/\/jira.skyeng.tech\/images\/icons\/priorities\/.*svg/gm)[i]}" style="width:20px; height:25px;" title="Приоритеты: ⛔ - Blocker, полностью залитая красная стрелка вверх - Critical, три красные стрелки вверх - Major, три синие вниз - Minor, ⭕ - Trivial">` + 
-										' ' + '<span class="newcount" style="width:20px; margin-left: 5px; background:#3CB371; padding:2px; padding-left:6px; font-weight:700; border-radius:10px;">' + 
-										`${rezissuetable.issueTable.table.match(/(">.)*?([0-9]+)\n/gm)[i]}` + '</span>' + 
-										`<a name="buglinks" href="https://jira.skyeng.tech/browse/${issueKeys[Number(pageSwArr[d].getAttribute('value'))+i]}" onclick="" target="_blank" style="margin-left:5px; color: #ffe4c4">` +
-										temporarka + '</a>' + 
-										`<span name="issueIds" style="display:none">${rezissuetable.issueTable.issueIds[Number(pageSwArr[d].getAttribute('value')) + i]}` + '</span>' + 
-										'<span class = "jiraissues" style="margin-left: 10px; cursor: pointer">💬</span>' + 
-										'<span class = "refreshissues" style="color:#ADFF2F; margin-left: 5px; cursor: pointer">&#69717;&#120783;</span>' + 
-										'<span name="addtofavourites" style="cursor:pointer;" title="Добавить задачу в Избранное">🤍</span>' + '</br>'
+									if (currentNumber && issueKeys[i] !== undefined) {
+													function filterItems(item, index) {
+														return index % 2 !== 0 ? item : null;
+													}
 
+													function replaceItem(item) {
+														return item.replace('">', ' – ');
+													}
 
-										}
-										}
-									}
+													const matchedItems = rezissuetable.issueTable.table.match(/(\w+-\d+">.*?).<\/a>/gmi).filter(filterItems);
+													const searchText = document.getElementById('testJira').value;
+													const isMatched = replaceItem(matchedItems[i]).toLowerCase().indexOf(searchText.toLowerCase()) !== -1;
 
-									document.getElementById('issuetable').innerHTML = issues;
-									
+													if (isMatched) {
+														const replacePattern = new RegExp(searchText, 'i');
+														const replaceValue = `<span style="color:MediumSpringGreen; font-weight:700; text-shadow:1px 2px 5px rgb(0 0 0 / 55%);">${searchText.toUpperCase()}</span>`;
+														temporarka = replaceItem(matchedItems[i]).replace(replacePattern, replaceValue);
+													} else {
+														temporarka = replaceItem(matchedItems[i]);
+													}
+
+													issues += '<span style="color: #00FA9A">&#5129;</span>' + 
+														`<img src="${rezissuetable.issueTable.table.match(/https:\/\/jira.skyeng.tech\/images\/icons\/priorities\/.*svg/gm)[i]}" style="width:20px; height:25px;" title="Приоритеты: ⛔ - Blocker, полностью залитая красная стрелка вверх - Critical, три красные стрелки вверх - Major, три синие вниз - Minor, ⭕ - Trivial">` + 
+														' ' + '<span class="newcount" style="width:20px; margin-left: 5px; background:#3CB371; padding:2px; padding-left:6px; font-weight:700; border-radius:10px;">' + currentNumber + '</span>' + 
+														`<a name="buglinks" href="https://jira.skyeng.tech/browse/${issueKeys[i]}" onclick="" target="_blank" style="margin-left:5px; color: #ffe4c4">` + temporarka + '</a>' + 
+														`<span name="issueIds" style="display:none">${rezissuetable.issueTable.issueIds[i]}</span>` + 
+														'<span class = "jiraissues" style="margin-left: 10px; cursor: pointer">💬</span>' + 
+														'<span class = "refreshissues" style="color:#ADFF2F; margin-left: 5px; cursor: pointer">&#69717;&#120783;</span>' + 
+														'<span name="addtofavourites" style="cursor:pointer;" title="Добавить задачу в Избранное">🤍</span>' + 
+														'</br>';
+												} else if (issueKeys[i] !== undefined) {
+													function filterItems(item, index) {
+														return index % 2 !== 0 ? item : null;
+													}
+
+													function replaceItem(item) {
+														return item.replace('">', ' – ');
+													}
+
+													const matchedItems = rezissuetable.issueTable.table.match(/(\w+-\d+">.*?).<\/a>/gmi).filter(filterItems);
+													const searchText = document.getElementById('testJira').value;
+													const isMatched = replaceItem(matchedItems[i]).toLowerCase().indexOf(searchText.toLowerCase()) !== -1;
+
+													if (isMatched) {
+														const replacePattern = new RegExp(searchText, 'i');
+														const replaceValue = `<span style="color:MediumSpringGreen; font-weight:700; text-shadow:1px 2px 5px rgb(0 0 0 / 55%);">${searchText.toUpperCase()}</span>`;
+														temporarka = replaceItem(matchedItems[i]).replace(replacePattern, replaceValue);
+													} else {
+														temporarka = replaceItem(matchedItems[i]);
+													}
+
+													issues += '<span style="color: #00FA9A">&#5129;</span>' + 
+														`<img src="${rezissuetable.issueTable.table.match(/https:\/\/jira.skyeng.tech\/images\/icons\/priorities\/.*svg/gm)[i]}" style="width:20px; height:25px;" title="Приоритеты: ⛔ - Blocker, полностью залитая красная стрелка вверх - Critical, три красные стрелки вверх - Major, три синие вниз - Minor, ⭕ - Trivial">` + 
+														' ' + `<a name="buglinks" href="https://jira.skyeng.tech/browse/${issueKeys[i]}" onclick="" target="_blank" style="margin-left:5px; color: #ffe4c4">` + temporarka + '</a>' + 
+														`<span name="issueIds" style="display:none">${rezissuetable.issueTable.issueIds[i]}</span>` + 
+														'<span class = "jiraissues" style="margin-left: 10px; cursor: pointer">💬</span>' + '</br>';
+												} else {
+													console.error("Не удалось найти соответствие для индекса: " + i);
+												}
+											}
+
+											document.getElementById('issuetable').innerHTML = issues;
+																			
 									let barray = document.querySelectorAll('.jiraissues');
 									for (let j = 0; j < barray.length; j++) {
 										barray[j].onclick = function () {
