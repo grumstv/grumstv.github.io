e878d73c1f08b1935967cbaff3d1582072510107
grumstv <grumstv@gmail.com>
2023-09-25 15:04:43 +0300

    Update WFMHelper.js

--- CuratorsHelper/Modules/WFMHelper.js
+++ CuratorsHelper/Modules/WFMHelper.js
@@ -13,9 +13,9 @@ var win_WFMHelper = `<div">
 										<li><label title="⏰ 07:00-19:00 🍔 12:15-13:30"><input type="checkbox" name="upper" value="Тарасова Наталья">Тарасова Наталья</label></li>								
 										<li><label title="⏰ 08:00-20:00 🍔 13:45-15:00"><input type="checkbox" name="upper" value="Зубарев Дмитрий">Зубарев Дмитрий</label></li>
 										<li><label title="⏰ 08:00-20:00 🍔 14:45-16:00"><input type="checkbox" name="upper" value="Радченко Александр">Радченко Александр</label></li>	
-										<li><label title="⏰ 11:00-23:00 🍔 15:45-17:00"><input type="checkbox" name="lower" value="Лукач Дарья">Лукач Дарья</label></li>
-										<li><label title="⏰ 10:00-22:00 🍔 14:45-16:00"><input type="checkbox" name="lower" value="Цой Анатолий">Цой Анатолий</label></li>										
-										<li><label title="⏰ 12:00-00:00 🍔 16:45-18:00"><input type="checkbox" name="upper" value="Артемьев Сергей">Артемьев Сергей</label></li>
+										<li><label title="⏰ 11:00-23:00 🍔 15:45-17:00"><input type="checkbox" name="lower" value="Лукач Дарья">Лукач Дарья</label></li>								
+										<li><label title="⏰ 12:00-00:00 🍔 16:45-18:00"><input type="checkbox" name="upper" value="Цой Анатолий">Цой Анатолий</label></li>
+										<li><label title=""><input type="checkbox" name="5days" value="---">---</label></li>
 									<li><label name="selectAllWorkersByChanges" style="background-color:DeepSkyBlue; text-align:center; font-weight:700; color:black; padding:2px; padding-left:10px; padding-right:10px; border: 1px solid black;">2/2 | Нижняя смена</label></li>
 										<li><label title="⏰ 07:00-19:00 🍔 12:45-14:00"><input type="checkbox" name="lower" value="Шеремет Александр">Шеремет Александр</label></li>									
 										<li><label title="⏰ 09:00-21:00 🍔 14:45-16:00"><input type="checkbox" name="upper" value="Изотеев Даниил">Изотеев Даниил</label></li>
